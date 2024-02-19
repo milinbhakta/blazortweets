8897219e7f1994fe056e3dc67afedd7695211c73
Milinbhakta <desire2dosomething@gmail.com>
2024-02-19 18:27:59 -0400

    Add Tweet list feature using MediatR

---
+++ Features/Tweets/List.cs
@@ -0,0 +1,37 @@
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace blazortweets.Features.Tweets
+{
+    public class List
+    {
+        public class Query : IRequest<Model>
+        {
+        }
+
+        public class Model
+        {
+            public List<string> Tweets { get; set; }
+        }
+
+        public class QueryHandler : IRequestHandler<Query, Model>
+        {
+            public async Task<Model> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return new Model
+                {
+                    Tweets = new List<string>
+                {
+                    "One from the server",
+                    "Two from the server",
+                    "Three from the server",
+                    "Four"
+                }
+                };
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using blazortweets.Data;
+using MediatR;
+using System.Reflection;
 
 namespace blazortweets
 {
@@ -29,6 +31,7 @@ namespace blazortweets
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
