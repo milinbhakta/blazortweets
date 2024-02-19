4973965b0b2c53c7a23aca696d291a17f2acd7e5
Milinbhakta <desire2dosomething@gmail.com>
2024-02-19 18:38:58 -0400

    Remove unused code and files

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,7 +24,6 @@ namespace blazortweets
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddSingleton<Store>();
         }
